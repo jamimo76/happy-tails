c475be9403e610885b94f26ade107d8d413837f5
Jami Orris <81092462+jamimo76@users.noreply.github.com>
2022-05-03 05:45:33 -0400

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,9 @@ import HaroldParker from "../app/assets/img/haroldparker.jpg";
 const Header = () => {
   return (
     <Navbar light color="success" sticky="top" expand="md">
-      <NavbarBrand href="/">
-        <img src={HaroldParker} alt="Harold Parker" size="auto"/>
+      <NavbarBrand>
+        <img src={HaroldParker} alt="Harold Parker" size="auto" />
+        <h1>Happy Tail Trails</h1>
       </NavbarBrand>
     </Navbar>
   );
