a65f9b5c7d7c814fb1de0277f6599807559ce49f
Jami Orris <81092462+jamimo76@users.noreply.github.com>
2022-07-19 05:24:03 -0400

    images rendering

--- src/app/shared/LOCATIONS.js
+++ src/app/shared/LOCATIONS.js
@@ -1,22 +1,25 @@
+import haroldParkerImg from "../assets/img/haroldparker.jpg";
+import salemTownForestImg from "../assets/img/salemtownforest.jpg";
+import winnekenniRiseImg from "../assets/img/winnekenni rise.jpg";
 
 export const LOCATIONS = [
   {
     id: 0,
     name: "Harold Parker State Forest",
-    image: "assets/images/haroldparker.jpg",
+    image: haroldParkerImg,
     description:
       "State Forest located in the towns of Andover, North Andover, Middleton, and North Reading."
   },
   {
     id: 1,
     name: "Salem Town Forest",
-    image: "assets/images/salemtownforest.jpg",
+    image: salemTownForestImg,
     description: "Town forest of Salem, New Hampshire."
   },
   {
     id: 2,
     name: "Winnekenni Park",
-    image: "assets/images/winnekenni rise.jpg",
+    image: winnekenniRiseImg,
     description:
       "Algonquin for 'very beautiful' located in the heart of Haverhill, MA."
   }
